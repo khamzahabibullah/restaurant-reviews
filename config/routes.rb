26f377e98846e5500d622f1a69bfac9ce3a6d680
Khamza Habibullah <habibullahkhamza@gmail.com>
2022-02-16 10:46:12 +0400

    whole app done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  # collection
+  # member
+  #/restaurants/top
+  resources :restaurants do
+    collection do
+      # get '/restaurants/top', to: 'restaurants#top'
+      get :top
+    end
+
+    member do
+      # /restaurnts/:id/chef
+      get :chef
+    end
+
+    resources :reviews, only: [:new, :create]
+
+  end
+
+  resources :reviews, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying schema"
+
+Restaurant.destroy_all
+
+puts "Generating seed..."
+
+100.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    rating: rand(1..5),
+    chef_name: ["Gordon Ramsey", "Rattatoui", "Akmal", "Salt Bae", "Jamie Oliver"].sample
+  )
+end
+
+puts "Generated all seeds"
